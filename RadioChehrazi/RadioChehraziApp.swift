ea3817ac0a69cdcb5fe0ce0f63f7f28715b50b8a
hosein alimoradi <ho.alimoradi@secnerd.ir>
2021-10-02 22:41:06 +0330

    Initial Commit

---
+++ RadioChehrazi/RadioChehraziApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioChehraziApp.swift
+//  RadioChehrazi
+//
+//  Created by Hosein Alimoradi on 7/10/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct RadioChehraziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
